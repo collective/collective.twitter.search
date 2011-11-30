0f033109144f3bf22da5883acf0f6fe85153debe
Franco Pellegrini <frapell@gmail.com>
2011-11-29 22:25:21 -0300

    Initial commit for the collective.twitter.search product

---
+++ src/collective/twitter/search/interfaces.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class ITwitterSearchLayer(IDefaultPloneLayer):
+    """ Browser layer for twitter search. """
---
+++ src/collective/twitter/search/portlet/twsearch.py
@@ -0,0 +1,209 @@
+from zope.interface import implements
+from zope.interface import alsoProvides
+
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+
+from zope import schema
+from zope.component import getUtility
+from Products.CMFCore.utils import getToolByName
+from zope.formlib import form
+from zope.schema.vocabulary import SimpleVocabulary
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.registry.interfaces import IRegistry
+from zope.schema.interfaces import IContextSourceBinder
+
+from collective.twitter.search import _
+
+import DateTime
+import twitter
+
+def TwitterAccounts(context):
+    registry = getUtility(IRegistry)
+    accounts = registry['collective.twitter.accounts']
+    if accounts:
+        vocab = accounts.keys()
+    else:
+        vocab = []
+
+    return SimpleVocabulary.fromValues(vocab)
+
+
+alsoProvides(TwitterAccounts, IContextSourceBinder)
+
+class ITwitterSearchPortlet(IPortletDataProvider):
+    """A portlet
+
+    It inherits from IPortletDataProvider because for this portlet, the
+    data that is being rendered and the portlet assignment itself are the
+    same.
+    """
+
+    header = schema.TextLine(title=_(u'Header'),
+                                    description=_(u"The header for the portlet. Leave empty for none."),
+                                    required=False)
+
+    tw_account = schema.Choice(title=_(u'Twitter account'),
+                               description=_(u"Which twitter account to use."),
+                               required=True,
+                               source=TwitterAccounts)
+
+    search_string = schema.TextLine(title=_(u'Search string'),
+                                    description=_(u"What to search."),
+                                    required=True)
+
+    show_avatars = schema.Bool(title=_(u'Show avatars'),
+                               description=_(u"Show people's avatars."),
+                               required=False)
+
+    max_results =  schema.Int(title=_(u'Maximum results'),
+                               description=_(u"The maximum results number."),
+                               required=True,
+                               default=20)
+
+
+class Assignment(base.Assignment):
+    """Portlet assignment.
+
+    This is what is actually managed through the portlets UI and associated
+    with columns.
+    """
+
+    implements(ITwitterSearchPortlet)
+
+    header = u""
+    tw_account = u""
+    search_string = u""
+    show_avatars = u""
+    max_results = 20
+    
+    def __init__(self,
+                 tw_account,
+                 search_string,
+                 max_results,
+                 header=u"",
+                 show_avatars=u""):
+                     
+        self.header = header
+        self.tw_account = tw_account
+        self.search_string = search_string
+        self.show_avatars = show_avatars
+        self.max_results = max_results
+
+    @property
+    def title(self):
+        """This property is used to give the title of the portlet in the
+        "manage portlets" screen.
+        """
+        return _(u"Twitter search portlet")
+
+
+
+class Renderer(base.Renderer):
+    """Portlet renderer.
+
+    This is registered in configure.zcml. The referenced page template is
+    rendered, and the implicit variable 'view' will refer to an instance
+    of this class. Other methods can be added and referenced in the template.
+    """
+    
+    render = ViewPageTemplateFile('twsearch.pt')
+
+
+    def getHeader(self):
+        """
+        Returns the header for the portlet
+        """
+        return self.data.header
+
+    def getSearchResults(self):
+        registry = getUtility(IRegistry)
+        accounts = registry['collective.twitter.accounts']
+
+        ploneutils = getToolByName(self.context, 'plone_utils')
+        
+        account = accounts.get(self.data.tw_account, {})
+
+        if account:
+            tw =  twitter.Api(consumer_key = account.get('consumer_key'),
+                              consumer_secret = account.get('consumer_secret'),
+                              access_token_key = account.get('oauth_token'),
+                              access_token_secret = account.get('oauth_token_secret'),)
+
+            search_str = self.data.search_string
+            max_results = self.data.max_results
+            
+            results = tw.GetSearch(search_str, per_page=max_results)
+            return results
+            
+                      
+        else:
+            msg = _("Could not search in Twitter, seems the account %s "
+                    "was removed from the list of authorized accounts for this "
+                    "site." % self.data.tw_account)
+                    
+            ploneutils.addPortalMessage( msg )
+            self.context.REQUEST.response.redirect(obj.absolute_url())
+
+
+    def getTweet(self, result):
+        # The API includes the username in the tweet's text, we need to
+        # remove it here. Also, we need to make URLs, hastags and users,
+        # clickable.
+        URL_TEMPLATE ="""
+        <a href="%s" target="blank_">%s</a>
+        """
+        HASHTAG_TEMPLATE ="""
+        <a href="http://twitter.com/#!/search?q=%s" target="blank_">%s</a>
+        """
+        USER_TEMPLATE ="""
+        <a href="http://twitter.com/#!/%s" target="blank_">%s</a>
+        """
+        
+        full_text = result.GetText()
+        split_text = full_text.split(' ')
+
+        # First, lets get rid of the username
+        split_text = split_text[1:]
+        
+        # Now, lets fix links, hashtags and users
+        for index, word in enumerate(split_text):
+            if word.startswith('@'):
+                # This is a user
+                split_text[index] = USER_TEMPLATE%(word[1:],word)
+            elif word.startswith('#'):
+                # This is a hashtag
+                split_text[index] = HASHTAG_TEMPLATE%("%23"+word[1:],word)
+            elif word.startswith('http'):
+                # This is a hashtag
+                split_text[index] = URL_TEMPLATE%(word,word)
+                    
+        return "<p>%s</p>"%' '.join(split_text)
+
+    def getDate(self, result):
+        # Returns human readable date for the tweet
+        date = DateTime.DateTime(result.GetCreatedAt())
+
+        return date
+        
+class AddForm(base.AddForm):
+    """Portlet add form.
+
+    This is registered in configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display. The create() method actually
+    constructs the assignment that is being added.
+    """
+    form_fields = form.Fields(ITwitterSearchPortlet)
+
+    def create(self, data):
+        return Assignment(**data)
+
+class EditForm(base.EditForm):
+    """Portlet edit form.
+
+    This is registered with configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display.
+    """
+    form_fields = form.Fields(ITwitterSearchPortlet)
