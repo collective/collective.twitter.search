e949e7544eb28f2a535f7dc3136e6f1463db5b63
hvelarde <hector.velarde@gmail.com>
2011-12-24 09:36:43 -0600

    i18n update

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -6,7 +6,7 @@ I18NDOMAIN="collective.twitter.search"
 # All on one line normally:
 bin/i18ndude rebuild-pot --pot src/collective/twitter/search/locales/${I18NDOMAIN}.pot \
     --create ${I18NDOMAIN} \
-   .
+    src/collective/twitter/search
 
 # Synchronise the resulting .pot with all .po files
 for po in src/collective/twitter/search/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
