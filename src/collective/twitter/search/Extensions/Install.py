09dc61d1def38fced558886d0ce441339650277c
Franco Pellegrini <frapell@gmail.com>
2012-01-14 16:20:59 -0300

    Separated initial and default profiles, to properly install collective.twitter.accounts

--- src/collective/twitter/search/Extensions/Install.py
+++ src/collective/twitter/search/Extensions/Install.py
@@ -1,6 +1,15 @@
 from Products.CMFCore.utils import getToolByName
 
-def uninstall(portal):
+def install(portal, reinstall=False):
     setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile('profile-collective.twitter.search:uninstall')
+    if not reinstall:
+        setup_tool.runAllImportStepsFromProfile('profile-collective.twitter.search:initial')
+
+    setup_tool.runAllImportStepsFromProfile('profile-collective.twitter.search:default')
     return "Ran all uninstall steps."
+
+def uninstall(portal, reinstall=False):
+    if not reinstall:
+        setup_tool = getToolByName(portal, 'portal_setup')
+        setup_tool.runAllImportStepsFromProfile('profile-collective.twitter.search:uninstall')
+        return "Ran all uninstall steps."
